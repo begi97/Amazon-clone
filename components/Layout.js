c0a066e2d967b949bd7232de04b7c7f3a2918e50
begi97 <badirovbahruz@gmail.com>
2023-04-29 16:24:18 +0300

    new changes regardnig login page added

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { Store } from "@/utils/Store";
 
 export const Layout = ({ children, title }) => {
+  const { state, dispatch } = useContext(Store);
+
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+
   return (
     <>
       <Head>
         <title>{title ? title + " - Amazon" : "Amazon"}</title>
         <meta name="description" content="Ecommerce Website" />
-        <link rel="stylesheet" href="/favicon.ico" />
+        {/* <link rel="stylesheet" href="/favicon.ico" /> */}
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header>
@@ -18,7 +28,12 @@ export const Layout = ({ children, title }) => {
             </Link>
             <div>
               <Link href="/cart" className="p-2">
-                Cart
+                Cart{" "}
+                {cartItemsCount > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cartItemsCount}
+                  </span>
+                )}
               </Link>
               <Link href="/login" className="p-2">
                 Login
